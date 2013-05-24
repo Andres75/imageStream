e0670999b70d068a3d5772b7f01a5fbd7b0d7a2a
Andy Menegazzo <andres.menegazzo@adtrak.co.uk>
2013-05-24 16:40:50 +0100

    Third commit

--- app/scan/scan.php
+++ app/scan/scan.php
@@ -1,10 +1,35 @@
+<pre>
 <?php
+$ritit = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(SCAN_FOLDER), RecursiveIteratorIterator::CHILD_FIRST);
+$r = array();
+foreach ($ritit as $splFileInfo) {
+   $path = $splFileInfo->isDir()
+         ? array($splFileInfo->getFilename() => array())
+         : array($splFileInfo->getFilename());
 
-$path = realpath(SCAN_FOLDER);
-
-$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
-foreach($objects as $name => $object){
-    echo "$name\n<br>";
+   for ($depth = $ritit->getDepth() - 1; $depth >= 0; $depth--) {
+       $path = array($ritit->getSubIterator($depth)->current()->getFilename() => $path);
+   }
+   $r = array_merge_recursive($r, $path);
 }
+$rootArray = $r;
 
+foreach ($rootArray as $rootArrayKey => $val){
+	
+	//echo $arrayElement;
+	if (gettype($val) == "array"){
+		
+		echo "  [DIR] - ".$rootArrayKey."<br>";
+		$subArrayCategory = $val;
+		foreach ($subArrayCategory as $subArrayElement){
+			
+			echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$subArrayElement."<br>";
+			
+		}
+		
+	}
+	//if (gettype($entity_name) == 'Array') echo "Array";
+	
+}
 ?>
+</pre>
